88a72bc1d1d93c4c78c134bc5d6a645e2791eebd
leonel <c.leonelortiz10@gmail.com>
2023-06-01 00:25:16 -0300

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,11 @@
 import { useRef, useState } from 'react'
 import '../App.css'
 import logo2 from '../assets/logobarber2.svg'
-import cejas from '../assets/cejas1.webp'
+import cejas from '../assets/cejas1.jpg'
+import corte from '../assets/corte1.jpg'
+import barba from '../assets/barba1.jpg'
+import peinado from '../assets/peinado1.jpg'
+import tintura from '../assets/tintura1.jpg'
 import whatsapp from '../assets/whatsapp.png'
 
 export function Home() {
@@ -18,32 +22,34 @@ export function Home() {
 }
 
 export function Services() {
-  const service=useRef(null);
-  let [mostrar,setMostrar]=useState(false)
+  const service = useRef(null);
+  let [mostrar, setMostrar] = useState(false)
   document.addEventListener("scroll", () => {
-    if(service.current.getBoundingClientRect().top<window.innerHeight/1.7)
-    setMostrar(true)
+    if (service.current.getBoundingClientRect().top < window.innerHeight / 1.7)
+      setMostrar(true)
     else
-    setMostrar(false)
+      setMostrar(false)
   })
   return (
-    <div id='Services' className={((mostrar==true) ? "animation" : "Services")} ref={service}>
+    <div id='Services' className={((mostrar == true) ? "animation" : "Services")} ref={service}>
       <h2 className='text-center m-auto mt-5'>Servicios</h2>
 
       <ul className='container-fluid row d-flex justify-content-center mx-auto '>
         <li className='card-1 col-sm-3 p-3 m-2'>
           <h3 className='text-center'>Corte de cabello</h3>
-          <img src={cejas} className='w-100' />
+          <img src={corte} className='w-100' />
           <h4 className='text-success text-end'>price</h4>
           <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit. A voluptas commodi eligendi suscipit cumque, nihil rep</h4>
         </li>
         <li className='card-1 col-sm-3 p-3 m-2'>
           <h3 className='text-center'>tintura</h3>
+          <img src={tintura} className='w-100' />
           <h4 className='text-success text-end'>price</h4>
           <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit. A voluptas commodi eligendi suscipit cumque, nihil rep</h4>
         </li>
         <li className='card-1 col-sm-3 p-3 m-2'>
           <h3 className='text-center'>barba</h3>
+          <img src={barba} className='w-100' />
           <h4 className='text-success text-end'>price</h4>
           <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit. A voluptas commodi eligendi suscipit cumque, nihil rep</h4>
         </li>
@@ -55,6 +61,7 @@ export function Services() {
         </li>
         <li className='card-1 col-sm-3 p-3 m-2'>
           <h3 className='text-center'>peinados</h3>
+          <img src={peinado} className='w-100' />
           <h4 className='text-success text-end'>price</h4>
           <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit. A voluptas commodi eligendi suscipit cumque, nihil rep</h4>
         </li>
@@ -73,16 +80,22 @@ export function Reservation() {
             <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
           </div>
           <div className="modal-body">
-            <form className='d-flex flex-column'>
-              <input type='text' placeholder='Nombre' />
-              <input type='email' placeholder='Email' />
-              <input type='phone' placeholder='Telefono' />
-              <input type='datetime-local' />
+            <form className='d-flex flex-column' action="https://formsubmit.co/cirojuarez114@gmail.com" method="POST" onSubmit={() => {
+                alert("su turno fue reservado con exito.");
+                window.location.reload()
+            } 
+            }>
+              <input type='text' placeholder='Nombre' name='Nombre' />
+              <input type='email' placeholder='Email' name='Email' />
+              <input type='phone' placeholder='Telefono' name='Telefono' />
+              <input type='datetime-local' name='Turno' />
+              <div className="modal-footer">
+                <button type="button" className="btn btn-cerrar" data-bs-dismiss="modal">Cerrar</button>
+                <button type="submit" className="btn btn-reservar">reservar</button>
+              </div>
+              <input type='hidden' name='_next' value={window.location.href}></input>
+              <input type='hidden' name='_captcha' value="false"></input>
             </form>
-          </div>
-          <div className="modal-footer">
-            <button type="button" className="btn btn-cerrar" data-bs-dismiss="modal">Cerrar</button>
-            <button type="button" className="btn btn-reservar">reservar</button>
           </div>
         </div>
       </div>
@@ -93,7 +106,7 @@ export function Reservation() {
 export function Contacts() {
 
   return (
-    <a href=''>
+    <a href='https://wa.me/543813867770'>
       <img src={whatsapp} className='wp'></img>
     </a>
   )
